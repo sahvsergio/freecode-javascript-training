fc3a198b3e8f24562e8c8614cf664ff0fa9732e7
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-05-01 20:41:27 -0500

    let allows to reassign the variable

--- basic_js.js
+++ basic_js.js
@@ -1,1 +1,8 @@
+// variable initialization and assignment
+
+//with let it can be reassgined
 let character="Hello";
+//printing the variable to the console
+console.log(character)
+
+character= 'World'
