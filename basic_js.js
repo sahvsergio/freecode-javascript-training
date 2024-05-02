c4c76710434dd6922bbc483bd6c9bcfb6dc4d787
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-05-01 21:06:08 -0500

    camel case example

--- basic_js.js
+++ basic_js.js
@@ -3,6 +3,10 @@
 //with let it can be reassgined
 let character="Hello";
 //printing the variable to the console
-console.log(character)
+console.log(character);
 
-character= 'World'
+character= 'World';
+console.log(character);
+
+//camel case is used in Javascript for  variables with more than 1 word
+let secondCharacter;
