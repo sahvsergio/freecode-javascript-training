7d67b79f4284c842c668d2d91e4a6706da329e2a
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-05-05 21:02:18 -0500

    getting started with arrays

--- basic_js.js
+++ basic_js.js
@@ -23,5 +23,14 @@ let count=8;
 console.log(count + 1);
 console.log(count*3);
 console.log(count/2);
-console.log(count-2)
+console.log(count-2);
+
+//Arrays:
+// -they are declared using []
+// variable with empty array
+
+let rows= [];
+// order of values 
+rows=['Naomi',"Quincy","CamperChan"];
+
 
